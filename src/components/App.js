398e877c57924174c0340bf324c2e59d7148da39
Md Umar <mohammadumar389@gmail.com>
2023-02-01 16:54:20 +0530

    updated app

--- src/components/App.js
+++ src/components/App.js
@@ -17,13 +17,13 @@ function App() {
     <Navbar/>
     <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/sign-in" element={<Login />} />
-    <Route path="/secret-page" element={<Secret />} />
-    <Route path="/secret-form" element={<Form />} />
-    <Route path="/sign-up" element={<Register />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/contact" element={<Contact />} />
-    <Route path="/log-out" element={<Logout />} />
+    <Route path="sign-in" element={<Login />} />
+    <Route path="secret-page" element={<Secret />} />
+    <Route path="secret-form" element={<Form />} />
+    <Route path="sign-up" element={<Register />} />
+    <Route path="about" element={<About />} />
+    <Route path="contact" element={<Contact />} />
+    <Route path="log-out" element={<Logout />} />
     </Routes>
 
     </div>
