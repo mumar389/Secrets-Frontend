5b1fb8939b4529a23a2322b4215b0e7447075c35
Md Umar <mohammadumar389@gmail.com>
2023-02-01 17:58:35 +0530

    sdgdfg

--- src/components/App.js
+++ src/components/App.js
@@ -17,13 +17,13 @@ function App() {
     <Navbar/>
     <Routes>
     <Route exact path="/" element={<Home />} />
-    <Route exact path="sign-in" element={<Login />} />
-    <Route exact path="secret-page" element={<Secret />} />
-    <Route exact path="secret-form" element={<Form />} />
-    <Route exact path="sign-up" element={<Register />} />
-    <Route exact path="about" element={<About />} />
-    <Route exact path="contact" element={<Contact />} />
-    <Route exact path="log-out" element={<Logout />} />
+    <Route exact path="/sign-in" element={<Login />} />
+    <Route exact path="/secret-page" element={<Secret />} />
+    <Route exact path="/secret-form" element={<Form />} />
+    <Route exact path="/sign-up" element={<Register />} />
+    <Route exact path="/about" element={<About />} />
+    <Route exact path="/contact" element={<Contact />} />
+    <Route exact path="/log-out" element={<Logout />} />
     </Routes>
 
     </div>
